1667ec051b06dfd41092a86c4b70b905ac73f4c2
Ben Purinton <ben.purinton@gmail.com>
2023-05-30 13:54:41 -0800

    rebuild

---
+++ app/controllers/numbers_controller.rb
@@ -0,0 +1,25 @@
+class NumbersController < ApplicationController
+  def winners
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/woohoo" })
+  end
+
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/boo" })
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,68 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # PART 1: EACH IN ERB
+  # ===================
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get("/lottery/lucky", { :controller => "numbers", :action => "winners" })
+  get("/", { :controller => "numbers", :action => "winners" })
+
+  # Let users visit URLs:
+
+  # - /lottery/unlucky
+
+  get("/lottery/unlucky", { :controller => "numbers", :action => "losers" })
+
+  # PART 2: R→C→A→V DEBUGGING
+  # ======================
+
+  # Uncomment each route below ONE AT A TIME and debug.
+  # Do NOT uncomment more than one at a time, or you'll be dealing with multiple syntax errors at once.
+
+  get("/zodiacs/aries", { :controller => "fire", :action => "ram" })
+  get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
+  get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
+
+  get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
+  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+
+  get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
+  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+
+  get("/zodiacs/cancer", { :controller => "water", :action => "crab" })
+  get("/zodiacs/scorpio", { :controller => "water", :action => "scorpion" })
+  get("/zodiacs/pisces", { :controller => "water", :action => "fish" })
+
+  # PART 3: MORE R→C→A→V PRACTICE
+  # ==========================
+
+  # Let users visit the following URLs.
+  # See the target to find out what the interface should be — then match it.
+  # Follow the standard R→C→A→V procedure:
+  #  - Write the ROUTE for each URL:
+  #     - Pick a CONTROLLER to use (a new one if you want, or re-use an existing one).
+  #     - Pick an ACTION name.
+  #     - Define the action and pick a VIEW template.
+  #  - Wire it all up and make it say "hi". Test, READ THE ERROR MESSAGE, and debug if necessary.
+  #  - Add some static HTML to mock up the response that you want to send back.
+  #  - Write the Ruby to make it dynamic, and work properly.
+
+  get("/roll/1/6", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/2/6", { :controller => "rolling", :action => "roll_2_6"})
+  get("/roll/3/6", { :controller => "rolling", :action => "roll_3_6"})
+  get("/roll/4/6", { :controller => "rolling", :action => "roll_4_6"})
+  get("/roll/5/6", { :controller => "rolling", :action => "roll_5_6"})
+  get("/roll/6/6", { :controller => "rolling", :action => "roll_6_6"})
+
+  get("/roll/1/4", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/2/4", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/3/4", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/4/4", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/1/8", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/2/8", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/1/10", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/2/10", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/1/20", { :controller => "rolling", :action => "roll_1_6"})
+  get("/roll/2/20", { :controller => "rolling", :action => "roll_2_20"})
+
 end
